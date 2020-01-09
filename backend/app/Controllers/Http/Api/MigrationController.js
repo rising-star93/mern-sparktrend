b58b65ba3033fd87ea4523c87bae6a8b3959ff7b
rising-star93 <cody.dev.93@gmail.com>
2020-01-09 18:51:03 +0800

    added migrations and seeds

--- backend/app/Controllers/Http/Api/MigrationController.js
+++ backend/app/Controllers/Http/Api/MigrationController.js
@@ -1,7 +1,7 @@
 const BaseController = require('./BaseController')
 const Database = use('Database')
 const validators = require(`${use('Helpers').appRoot()}/database/validators`)
-
+const Hash = use('Hash')
 class MigrationController extends BaseController {
 
   async migrate({request, response}) {
@@ -13,7 +13,7 @@ class MigrationController extends BaseController {
     }
 
     await db.createCollection('users', validators.UserValidator)
-
+    await db.collection('users').createIndex({ "email": 1 }, { unique: true})
     console.log('collection users created')
 
     try {
@@ -23,6 +23,7 @@ class MigrationController extends BaseController {
     }
 
     await db.createCollection('instaaccounts', validators.InstaValidator)
+    await db.collection('instaaccounts').createIndex({"username": 1}, { unique: true })
     console.log('collection instaaccounts created')
 
     try {
@@ -33,6 +34,100 @@ class MigrationController extends BaseController {
 
     await db.createCollection('orders', validators.OrderValidator)
     console.log('collection orders created')
+
+
+
+    return response.json({
+      success: true
+    })
+  }
+
+  async seed({request, response}) {
+    const db = await Database.connect('mongodb')
+    await db.collection('users').remove({})
+    const user1 = await db.collection('users').insert({
+      name: 'bravemaster',
+      email: 'bravemaster619@hotmail.com',
+      password: await Hash.make('111111'),
+      country: 'China',
+      gender: 'male',
+      locale: 'en',
+      created_at: new Date(),
+      updated_at: new Date()
+    });
+    const user2 = await db.collection('users').insert({
+      name: 'devcrazy',
+      email: 'devcrazy@hotmail.com',
+      password: await Hash.make('111111'),
+      country: 'China',
+      gender: 'male',
+      locale: 'en',
+      created_at: new Date(),
+      updated_at: new Date()
+    });
+    console.log('two users inserted')
+    console.log(user1.insertedIds['0'])
+    await db.collection('instaaccounts').remove({})
+    await db.collection('instaaccounts').insert({
+      user_id: user1.insertedIds['0'].toString(),
+      username: 'instafashionfeeds',
+      profile_img: "https://scontent-lax3-2.cdninstagram.com/v/t51.2885-19/s150x150/18380890_110724382837223_774891342345011200_n.jpg?_nc_ht=scontent-lax3-2.cdninstagram.com&_nc_ohc=U7n98U2i0OoAX8BMvzo&oh=7b89d55f19d64f55b9f1a7b139ea47b9&oe=5E8D5BFA",
+      follower_count: 332097,
+      insights_picture: 'https://cdn.skedsocial.com/wp-content/uploads/2018/03/25141444/Screen-Shot-2018-03-25-at-10.12.28-PM.png',
+      verification_code: 'd73238e0',
+      verified: true,
+      type: 'business',
+      "demographics": {
+        "age": [
+          {"name": "13-17", "percent": 12},
+          {"name": "18-24", "percent": 45},
+          {"name": "25-34", "percent": 27},
+          {"name": "35-44", "percent": 10},
+          {"name": "45-54", "percent": 4},
+          {"name": "55-64", "percent": 1},
+          {"name": "65+", "percent": 1}
+        ],
+        "gender": [
+          {"name": "Men", "percent": 85},
+          {"name": "Women", "percent": 15}
+        ],
+        "country": [
+          {"name": "United States", "percent": 37},
+          {"name": "India", "percent": 7},
+          {"name": "United Kingdom", "percent": 5},
+          {"name": "Indonesia", "percent": 3},
+          {"name": "Brazil", "percent": 2}
+        ]
+      },
+      "product"   : {
+        "description" : "A stunningly beautiful page with a constant growth of followers, etc. ",
+        "banner_img": "https://tse3-mm.cn.bing.net/th/id/OIP.jNCbt_c_8vnq7sbWluCVnQHaCG?w=300&h=85&c=7&o=5&pid=1.7",
+        "niches"  : "Fashion & Style",
+        "categories" : [
+          {
+            "type"   :  "Single",
+            "pricing":  [
+              { "time": 6, "price": 15, "bio_price": 10 },
+              { "time": 12, "price": 20, "bio_price": 10 }
+            ]
+          },
+          {
+            "type"  : "Multiple",
+            "pricing": [
+              { "time": 12, "price": 30.5, "bio_price": 15 }
+            ]
+          },
+          {
+            "type"  : "Story",
+            "pricing": [
+              { "time": 24, "price": 40, "bio_price": 20}
+            ]
+          }
+        ],
+      },
+      "created_at": new Date,
+      "updated_at": new Date
+    })
 
     return response.json({
       success: true
--- backend/database/validators/orders.js
+++ backend/database/validators/orders.js
@@ -1,93 +1,97 @@
 module.exports = {
-  bsonType: 'object',
-  required: ['buyer_id', 'seller_id', 'insta_id', 'time', 'posts', 'price', 'subtotal', 'total'],
-  properties: {
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      required: ['buyer_id', 'seller_id', 'insta_id', 'time', 'posts', 'price', 'total'],
+      properties: {
 
-    buyer_id: { bsonType: 'objectId' },
+        buyer_id: { bsonType: 'objectId' },
 
-    seller_id : { bsonType: 'objectId' },
+        seller_id : { bsonType: 'objectId' },
 
-    product_id: { bsonType: 'objectId' },
+        insta_id: { bsonType: 'objectId' },
 
-    category: {
-        bsonType: 'string',
-        maxLength: 1000
-      },
+        category: {
+          bsonType: 'string',
+          maxLength: 1000
+        },
 
-    with_bio : { bsonType: 'bool' },
+        with_bio : { bsonType: 'bool' },
 
-    bio_url: {
-        bsonType: 'string',
-        maxLength: 65535
-      },
-
-    swipe_up_url: {
-        bsonType: 'string',
-        maxLength: 65535
-      },
-
-    start_from: { bsonType: 'date' },
-
-    caption: {
-        bsonType: 'string',
-        maxLength: 65535
-      },
-
-    additional_info: {
-        bsonType: 'string',
-        maxLength: 65535
-      },
-
-    posts: {
-        bsonType: 'array',
-        minItems: 1,
-        maxItems: 100,
-        items: {
+        bio_url: {
           bsonType: 'string',
           maxLength: 65535
-        }
-      },
+        },
 
-    time: {
-        bsonType: 'int',
-        minimum: 0
-      },
+        swipe_up_url: {
+          bsonType: 'string',
+          maxLength: 65535
+        },
 
-    price: {
-        bsonType: 'double',
-        minimum: 0
-      },
+        start_from: { bsonType: 'date' },
 
-    bio_price: {
-        bsonType: 'double',
-        minimum: 0
-      },
+        caption: {
+          bsonType: 'string',
+          maxLength: 65535
+        },
 
-    charge: {
-        bsonType: 'double',
-        minimum: 0
-      },
+        additional_info: {
+          bsonType: 'string',
+          maxLength: 65535
+        },
 
-    total: {
-        bsonType: 'double',
-        minimum: 0
-      },
+        posts: {
+          bsonType: 'array',
+          minItems: 1,
+          maxItems: 100,
+          items: {
+            bsonType: 'string',
+            maxLength: 65535
+          }
+        },
 
-    history: {
-        bsonType: 'object',
-        properties: {
-          created_at: { bsonType: 'date' },
-          accepted_at: { bsonType: 'date' },
-          started_at: { bsonType: 'date' },
-          completed_at: { bsonType: 'date' },
-          paid_at: { bsonType: 'date' },
-          rejected_at: { bsonType: 'date' },
-          refunded_at: { bsonType: 'date' },
-        }
-      },
+        time: {
+          bsonType: 'int',
+          minimum: 0
+        },
 
-    created_at: { bsonType: 'date' },
+        price: {
+          bsonType: 'decimal',
+          minimum: 0
+        },
 
-    updated_at: { bsonType: 'date' }
+        bio_price: {
+          bsonType: 'decimal',
+          minimum: 0
+        },
+
+        charge: {
+          bsonType: 'decimal',
+          minimum: 0
+        },
+
+        total: {
+          bsonType: 'decimal',
+          minimum: 0
+        },
+
+        history: {
+          bsonType: 'object',
+          properties: {
+            created_at: { bsonType: 'date' },
+            accepted_at: { bsonType: 'date' },
+            started_at: { bsonType: 'date' },
+            completed_at: { bsonType: 'date' },
+            paid_at: { bsonType: 'date' },
+            rejected_at: { bsonType: 'date' },
+            refunded_at: { bsonType: 'date' },
+          }
+        },
+
+        created_at: { bsonType: 'date' },
+
+        updated_at: { bsonType: 'date' }
+      }
+    }
   }
 }
