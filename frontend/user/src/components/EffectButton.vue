0632598444b1fcdae269bbdf7a35da3e8150c882
rising-star93 <cody.dev.93@gmail.com>
2020-01-11 04:07:12 +0800

    add waiting animation effect

---
+++ frontend/user/src/components/EffectButton.vue
@@ -0,0 +1,41 @@
+<template>
+    <button :class="{'disabled' : loading}" @click="onclick">
+        <bounce-loader v-if="loading" :loading="loading" :color="color" :size="30" margin="10px" sizeUnit="px"/>
+        <slot v-else></slot>
+    </button>
+</template>
+
+<script>
+    import { BounceLoader } from "@saeris/vue-spinners";
+
+    export default {
+        name: "EffectButton",
+        event:[
+            'click'
+        ],
+        props:{
+            loading : {
+                type: Boolean,
+                default: false
+            },
+            color   : {
+                type: String,
+                default: "#f7b9d1"
+            }
+        },
+        components: {
+            BounceLoader
+        },
+        methods:{
+            onclick(){
+                this.$emit("click");
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    button div{
+        margin: auto;
+    }
+</style>
