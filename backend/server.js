a8c94cf8d484ae657f8ccf2f42819b6ffd818078
rising-star93 <cody.dev.93@gmail.com>
2020-03-14 23:41:59 +0800

    http to https

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,17 @@
 */
 
 const { Ignitor } = require('@adonisjs/ignitor')
+const https = require('https')
+const fs = require('fs')
+
+const options = {
+  key: fs.readFileSync('path-to-key.key'),
+  cert: fs.readFileSync('path-to-key.crt')
+}
 
 new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
-  .fireHttpServer()
+  .fireHttpServer((handler) => {
+    return https.createServer(options, handler)
+  })
   .catch(console.error)
